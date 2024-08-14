be857ec61c09f462f436cd682a75db5f009b8abd
Frederico Swarthings <timscallon1@gmail.com>
2024-08-14 17:02:26 -0500

    updated models for armor, weapon, stat, and skill

--- models/Stat.js
+++ models/Stat.js
@@ -11,7 +11,23 @@ Stat.init(
       allowNull: false,
       autoIncrement: true
     },
+    //strength itself
     strength: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //strength mod int
+    strMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //strength save proficiency boolean component
+    strSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    //strength save proficiency int component
+    strSaveVal: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
@@ -19,7 +35,31 @@ Stat.init(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    dexMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dexSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    dexSaveVal: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     constitution: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    conMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    conSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    conSaveVal: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
@@ -27,11 +67,47 @@ Stat.init(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    intMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    intSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    intSaveVal: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     wisdom: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    wisMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    wisSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    wisSaveVal: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     charisma: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    chaMod: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    chaSaveProf: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    chaSaveVal: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
