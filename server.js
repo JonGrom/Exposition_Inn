f8a7fb87c6516b6704bf5e5d614efbc148f4f997
Frederico Swarthings <timscallon1@gmail.com>
2024-08-16 17:26:40 -0500

    YOUSHALLNOTPASS

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'YOUSHALLNOTPASS',
   cookie: {
     maxAge: 60 * 60 * 1000,
     httpOnly: true,
